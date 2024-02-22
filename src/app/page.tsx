df006a8a9cce0d24e7221fd7dd87bfc9c77ca2fe
PersianDavinci <persiandavinci@gmail.com>
2024-02-23 01:06:50 +0330

    add Home page button and style easy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,6 +14,12 @@ export default function Home() {
           Welcome To DigiMarket. Every asset on our platform is verified by our
           Team to ensure our highest quality standards.
         </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href="/products" className={buttonVariants()}>
+            Browse Trending Assets
+          </Link>
+          <Button variant={"ghost"}>Our Quality Promise &rarr;</Button>
+        </div>
       </div>
     </MaxWidthWrapper>
   );
