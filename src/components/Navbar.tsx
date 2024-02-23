83899296400c58e5c338cf5c56149fbe39a1cbc8
PersianDavinci <persiandavinci@gmail.com>
2024-02-23 16:25:00 +0330

    add Navbar.tsx basics

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+      <header className="relative bg-white">
+        <MaxWidthWrapper>
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+              {/* Mobile Nav */}
+
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/"></Link>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
