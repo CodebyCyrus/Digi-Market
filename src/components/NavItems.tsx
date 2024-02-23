fe0e9d99471a8ff721d5518734fb0dcb8d13d570
PersianDavinci <persiandavinci@gmail.com>
2024-02-23 18:25:50 +0330

    add some hooks(as a hook folder and it can reuseable to another project like closing nav bar just clicking outside the box = use-on-click-outside.ts) and fix image bug for nav

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,16 +1,23 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
   const isAnyOpen = activeIndex !== null;
 
+  // for closing when click outside the navigation box
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className="flex gap-4 h-full">
+    //   also pass navref to work outside click in this div
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
