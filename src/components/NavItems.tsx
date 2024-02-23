a7ca958d5cf335d150cc78caf9d42ced6e3d51f4
PersianDavinci <persiandavinci@gmail.com>
2024-02-23 17:27:15 +0330

    Add NavItems add config folder for images ad some basic demo content to config and added to navitems as a category and maped and add NavItem.tsx

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+"use client";
+
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
-  return <div>NavItems</div>;
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  const isAnyOpen = activeIndex !== null;
+
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+
+        const isOpen = i === activeIndex;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
