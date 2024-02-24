c988dc9221dc6ed77d3749fe2d1f9ffd56678b37
PersianDavinci <persiandavinci@gmail.com>
2024-02-24 17:04:43 +0330

    add cart title and itemCount basic coding and condition also add seprator from shadcn also add Utils function for currency for Cart also add logic to currency format utils and some img for Cart empty

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,25 @@
 "use client";
 
 import { ShoppingCart } from "lucide-react";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 const Cart = () => {
+  const itemCount = 0;
+
+  const fee = 1;
+
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -16,7 +32,53 @@ const Cart = () => {
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
-        <SheetHeader className="space-y-2.5 pr-6">Cart (0)</SheetHeader>
+        <SheetHeader className="space-y-2.5 pr-6">
+          <SheetTitle>Cart (0)</SheetTitle>
+        </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            <div className="flex flex-col pr-6">
+              {/* Todo: cart logic  */}
+              Cart Items
+            </div>
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-1.5 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span> {formatPrice(fee)} </span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                </div>
+              </div>
+
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link
+                    href="/cart"
+                    className={buttonVariants({ className: "w-full" })}
+                  >
+                    Continue To Checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div
+              aria-hidden="true"
+              className="relative mb-4 h-60 w-60 text-muted-foreground"
+            >
+              <Image src="/empty-cart.png" fill alt="Empty Shopping Cart" />
+            </div>
+          </div>
+        )}
       </SheetContent>
     </Sheet>
   );
